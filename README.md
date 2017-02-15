76ad183ad0be32ddbdfd05c5c249240456c1ae65
vicanso <vicansocanbico@gmail.com>
2017-02-15 22:31:41 +0800

    add composing text function

---
+++ README.md
@@ -0,0 +1,3 @@
+# font-metrics
+
+Get the composing of text
--- test/index.js
+++ test/index.js
@@ -1,26 +1,35 @@
 'use strict';
 const fs = require('fs');
-const _ = require('lodash');
+const path = require('path');
 const FontMetrics = require('..');
 
 
 function getChinese() {
-  const data = fs.readFileSync('../assets/chinese.dat', 'utf8');
+  const data = fs.readFileSync(path.join(__dirname, '../assets/chinese.dat'), 'utf8');
   const arr = data.split('\n');
-  const chinese = arr.map(item => item.substring(item.length - 1));
-
+  return arr.map(item => item.substring(item.length - 1));
 }
-function getRandomArticle() {
 
+function getRandomArticle() {
+  const chineseList = getChinese();
+  const start = Math.ceil(Math.random() * (chineseList.length / 2));
+  const end = Math.ceil(Math.random() * (chineseList.length / 2)) + start;
+  return chineseList.slice(start, end).join('');
 }
 
 describe('font-metrics', () => {
-  it('get content of page success', done => {
+  it('get content of page success', (done) => {
     const fontMetrics = new FontMetrics();
     fontMetrics.region = {
-      width: 360,
-      height: 640,
+      width: 375,
+      height: 667,
     };
-    fontMetrics.data = '';
+    fontMetrics.data = [
+      getRandomArticle(),
+      getRandomArticle(),
+      getRandomArticle(),
+    ].join('\n');
+    const composing = fontMetrics.getComposing();
+    done();
   });
 });
