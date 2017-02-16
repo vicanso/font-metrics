4601688d04d120a6136995b69a5c8ed227cb5702
vicanso <vicansocanbico@gmail.com>
2017-02-16 22:50:19 +0800

    improve font size cal

--- test/index.js
+++ test/index.js
@@ -1,21 +1,12 @@
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
+
 const FontMetrics = require('..');
-
-
-function getChinese() {
-  const data = fs.readFileSync(path.join(__dirname, '../assets/chinese.dat'), 'utf8');
-  const arr = data.split('\n');
-  return arr.map(item => item.substring(item.length - 1));
-}
-
-function getRandomArticle() {
-  const chineseList = getChinese();
-  const start = Math.ceil(Math.random() * (chineseList.length / 2));
-  const end = Math.ceil(Math.random() * (chineseList.length / 2)) + start;
-  return chineseList.slice(start, end).join('');
-}
+const text = fs.readFileSync(path.join(__dirname, '../assets/text.txt'), 'utf8');
+const template = fs.readFileSync(path.join(__dirname, '../assets/page-view.tpl'), 'utf8');
 
 describe('font-metrics', () => {
   it('get content of page success', (done) => {
@@ -24,12 +15,74 @@ describe('font-metrics', () => {
       width: 375,
       height: 667,
     };
-    fontMetrics.data = [
-      getRandomArticle(),
-      getRandomArticle(),
-      getRandomArticle(),
-    ].join('\n');
+    fontMetrics.data = text.trim();
     const composing = fontMetrics.getComposing();
+    assert.equal(composing.length, 6);
+    composing.forEach((item, i) => {
+      assert(item.content);
+      assert.equal(item.page, i);
+    });
     done();
+  });
+
+  it('create multi view of page', (done) => {
+    const devices = [
+      {
+        name: 'iPhone6P',
+        width: 414,
+        height: 736,
+      },
+      {
+        name: 'iPhone6',
+        width: 375,
+        height: 667,
+      },
+      {
+        name: 'iPhone5',
+        width: 320,
+        height: 568,
+      },
+      {
+        name: 'galaxyS5',
+        width: 360,
+        height: 640,
+      },
+      {
+        name: 'nexus5X',
+        width: 412,
+        height: 732,
+      },
+    ];
+    const contentHtml = devices.map((device) => {
+      const fontSize = 16;
+      const fontMetrics = new FontMetrics({
+        fontSize,
+      });
+      fontMetrics.region = {
+        width: device.width,
+        height: device.height,
+      };
+      fontMetrics.data = text.trim();
+      fontMetrics.getCustomFontSize = (ch) => {
+        if (ch.charCodeAt(0) < 0xff) {
+          return (fontSize / 2) + 2;
+        } else if (['“', ',', '”'].indexOf(ch) !== -1) {
+          return (fontSize / 2) + 1;
+        }
+        return 0;
+      }
+      const arr = fontMetrics.toHTML();
+      const html = fontMetrics.toHTML()
+        .map(item => `<div class="content">
+          <div class="bar">${device.name}</div>${item}
+        </div>`)
+        .join('');
+      return `<div class="${device.name}">${html}<div>`;
+    }).join('');
+    fs.writeFile(
+      path.join(__dirname, '../assets/page-view.html'),
+      template.replace('{CONTENT}', contentHtml),
+      done
+    );
   });
 });
